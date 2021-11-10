85eb314ede3570b20e40303ccb33847f9dfef6b5
Curtis.CN <cncurtismusic@gmail.com>
2021-11-10 15:33:07 +0800

    Update

--- 2002/11_10_2002/代码/mysqltest_2002(第二部分).sql
+++ 2002/11_10_2002/代码/mysqltest_2002(第二部分).sql
@@ -1,27 +1,39 @@
--- 14.从student表和score表查询出学生的学号，然后合并查询结果
-SELECT id
-FROM student 
-UNION
-SELECT stu_id 
-FROM 
-score; -- 去重
--- ---------------------------------
-SELECT
-id
-FROM
-student
-UNION ALL
-SELECT
-stu_id
-FROM 
-score; -- 不去重
--- 15.查询姓张或姓王的同学姓名，院系，考试科目及其成绩
-SELECT s.`NAME`,s.`department`,sc.`c_name`,sc.`grade`
-FROM student s,score sc
-WHERE s.`id`=sc.`stu_id`
-AND (s.`NAME` LIKE '张%' OR s.`NAME`LIKE '王%');
--- 16.查询都是湖南的学生姓名，年龄，院系，考试科目及成绩
-SELECT s.`NAME`,s.`birth`,s.`department`,sc.`c_name`,sc.`grade`
-FROM student s INNER JOIN score sc
-ON s.`id`=sc.`stu_id`
-WHERE s.`address` LIKE '湖南%';7
+USE db_2002;
+-- 准备测试数据
+DROP TABLE IF EXISTS salary_table;
+CREATE TABLE salary_table(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	POSITION VARCHAR(20),
+	salary INT
+);
+INSERT INTO salary_table(POSITION,salary) VALUES('Java',8000),('Java',8400),
+('Java',9000),('Python',6500),('Python',10000),('Python',8900);
+SELECT * FROM salary_table;
+-- 需求：查询薪水比岗位为Python薪水高的数据
+SELECT *
+FROM salary_table st
+WHERE st.`salary`>
+SOME(SELECT st.`salary`
+FROM salary_table st
+WHERE st.`position`='Python');
+-- 需求：查询薪水比==所有==岗位为Python的薪水高的数据
+SELECT *
+FROM salary_table st
+WHERE st.`salary`>
+ALL(SELECT st.`salary`
+FROM salary_table st
+WHERE st.`position`='Python');
+-- 需求：如果能够查询出有岗位为Python的数据，那么查询新水表的所有数据
+SELECT * FROM salary_table WHERE
+EXISTS(SELECT * 
+FROM salary_table st 
+WHERE st.position='Python');
+-- 需求：如果能够查询出有岗位为C语言的数据，那么查询新水表的所有数据
+SELECT * FROM salary_table WHERE
+EXISTS(SELECT * FROM salary_table st
+WHERE st.position='C语言');
+-- ---------------------------------------
+SELECT * FROM salary_table WHERE
+NOT EXISTS(SELECT * FROM salary_table st
+WHERE st.position='C语言');
+
