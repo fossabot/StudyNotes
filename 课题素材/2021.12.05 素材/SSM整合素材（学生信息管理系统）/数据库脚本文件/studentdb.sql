5658080c422ef9d341eea72a4770a5806998a3b2
curtisrecords <cncurtismusic@gmail.com>
2021-12-05 20:48:49 +0800

    Update 12.05 素材
    默认以解压方式上传了；压缩文档在目录内。

---
+++ 课题素材/2021.12.05 素材/SSM整合素材（学生信息管理系统）/数据库脚本文件/studentdb.sql
@@ -0,0 +1,112 @@
+/*
+SQLyog Ultimate v11.25 (64 bit)
+MySQL - 5.5.56 : Database - studentdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`studentdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `studentdb`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`id`,`username`,`password`,`name`) values (1,'admin','admin','超级管理员');
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `teacher_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teacher_course` (`teacher_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `course` */
+
+insert  into `course`(`id`,`name`,`teacher_id`) values (1,'面向对象程序设计',201),(2,'软件项目管理',202),(3,'基于ssm框架项目开发',203);
+
+/*Table structure for table `score` */
+
+DROP TABLE IF EXISTS `score`;
+
+CREATE TABLE `score` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` int(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `score` double(6,1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stu_score` (`student_id`),
+  KEY `course_score` (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `score` */
+
+insert  into `score`(`id`,`student_id`,`course_id`,`score`) values (1,101,1,100.0),(2,102,1,99.0),(3,102,2,88.0),(4,101,3,222.0),(5,101,2,44.0);
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `sex` int(20) DEFAULT NULL,
+  `clazz` varchar(20) DEFAULT NULL,
+  `birthday` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`password`,`sex`,`clazz`,`birthday`) values (102,'萧炎亮','0000',0,'13软件1','1995-08-01'),(103,'叶凡凯','0000',1,'13软件1','1994-01-23'),(104,'李牧尘','1111',0,'13软件1','1997-12-05'),(105,'刘红枫','0000',1,'13软件2','1995-11-15'),(106,'叶一','0000',1,'13软件2','1996-12-12');
+
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  `sex` int(11) DEFAULT NULL,
+  `birthday` varchar(20) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `professional` varchar(20) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `course_teacher` (`course_id`),
+  CONSTRAINT `course_teacher` FOREIGN KEY (`course_id`) REFERENCES `course` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher` */
+
+insert  into `teacher`(`id`,`name`,`password`,`sex`,`birthday`,`course_id`,`professional`,`salary`) values (201,'李青山','0000',1,'1965-01-01',1,'教授',3000),(202,'唐嫣然','0000',1,'1968-01-01',2,'教授',8000),(203,'萧玄茂','0000',1,'1978-01-01',3,'高级教师',9008);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
