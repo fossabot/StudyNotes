f9267b757f0523a4b8a6db0adf838c485029ffe3
curtisrecords <cncurtismusic@gmail.com>
2022-03-08 14:11:51 +0800

    Update 笔记更新.bat

--- 课题素材/同步工具/笔记更新.bat
+++ 课题素材/同步工具/笔记更新.bat
@@ -17,16 +17,16 @@ goto :eof
 :1
 echo 完全下载 (Clone)
 echo 确定继续吗?
-echo 1. 请保证此脚本文件是在桌面上的 且你之前Clone的学习文件也在桌面，否则可能导致脚本运行错误。
+echo 1. 可以选择其他磁盘，但要在同一文件夹内，否则可能导致脚本运行错误。
 echo 2. 请确认你是否已经安装Git.
+pause
 git clone https://gitee.com/CurtisCN/StudyNotes.git
 pause
 exit
 
 :2
 echo 增量更新 (Pull) 
-echo 1. 请保证此脚本文件是在桌面上的 且你之前Clone的学习文件也在桌面，否则可能导致脚本运行错误。
-echo 2. 请确认你是否已经安装Git.
+echo 1. 可以选择其他磁盘，但和Clone下来的文件要在同文件夹内，否则可能导致脚本运行错误。
 pause
 cd StudyNotes
 git pull origin main
